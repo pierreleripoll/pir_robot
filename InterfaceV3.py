ba4a0512cf387c0c8cf118e21c3cbbfb39787139
pierreleripoll <pierreripoll13@gmail.com>
2018-04-13 18:58:07 +0200

    affichage modifiable dans l'interpreteur / creation de l'OBJET affichage

--- InterfaceV3.py
+++ InterfaceV3.py
@@ -2,27 +2,25 @@ from tkinter import *
 
 
 
+class Affichage:
 
+    def __init__(self,Grille):
+        self.nbreDeCasesParLigne=Grille.nRows
+        self.hauteur=512
+        self.largeur=512
+        rempl=self.hauteur/self.nbreDeCasesParLigne
 
-def affichage(Grille):
-    nbreDeCasesParLigne=Grille.nRows
-    hauteur=512
-    largeur=512
-    rempl=hauteur/nbreDeCasesParLigne
+        self.fen=Tk()
+        self.can=Canvas(self.fen, width=self.largeur, height=self.hauteur, bg='ivory')
+        self.can.pack()
+        self.bstop=Button(self.fen, text='Fermer la fenêtre', command=self.fen.destroy)
+        self.bstop.pack()
+        for c in range(self.nbreDeCasesParLigne):
+                    self.can.create_line(c*rempl, 0,c*rempl,self.hauteur)
+                    self.can.create_line(0,c*rempl,self.largeur,c*rempl)
 
-    fen=Tk()
-    can=Canvas(fen, width=largeur, height=hauteur, bg='ivory')
-    can.pack()
-    bstop=Button(fen, text='Fermer la fenêtre', command=fen.destroy)
-    bstop.pack()
-    for c in range(nbreDeCasesParLigne):
-                can.create_line(c*rempl, 0,c*rempl,hauteur)
-                can.create_line(0,c*rempl,largeur,c*rempl)
-
-    fen.mainloop()
-
-def affChemin(Node,ajustage):
-    x=Node.x
-    y=Node.y
-    rempl=ajustage
-    can.create_rectangle(x,y,x+rempl,y+rempl,fill='red')
+    def affNode(self,Node):
+        x=Node.x
+        y=Node.y
+        rempl=self.hauteur/self.nbreDeCasesParLigne
+        self.can.create_rectangle(x,y,x+rempl,y+rempl,fill='red')
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from node import Node
 from grille import Grille
 from astar import AStar
 
+from InterfaceV3 import *
+
 lab = Grille(30,30);
 lab.setRect(15,15,10,2,".");
 lab.setRect(15,15,2,10,".");
