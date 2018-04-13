c0aaad910f9cfc873591c606cf5c7c8b7f19a60c
pierreleripoll <pierreripoll13@gmail.com>
2018-04-13 19:13:55 +0200

    change name function by easier names

--- InterfaceV3.py
+++ InterfaceV3.py
@@ -19,21 +19,21 @@ class Affichage:
                     self.can.create_line(c*rempl, 0,c*rempl,self.hauteur)
                     self.can.create_line(0,c*rempl,self.largeur,c*rempl)
 
-    def chgColorType(self,typeC,color):
+    def colorType(self,typeC,color):
         for column in self.grille.plan:
             for node in column:
                 if node.typeN == typeC:
-                    self.affNode(node,color)
+                    self.node(node,color)
 
-    def affNode(self,Node, color = "red"):
+    def node(self,Node, color = "red"):
         rempl=self.hauteur/self.nbreDeCasesParLigne
         x=Node.x*rempl
         y=Node.y*rempl
         self.can.create_rectangle(x,y,x+rempl,y+rempl,fill=color)
 
-    def affPath(self,path,color = None):
+    def path(self,path,color = None):
         for node in path:
             if color :
-                self.affNode(node,color)
+                self.node(node,color)
             else :
                 self.affNode(node)
