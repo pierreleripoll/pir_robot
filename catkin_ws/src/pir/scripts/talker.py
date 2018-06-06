33a5007003fee895d37dc57bfb1866277f6620cf
pierreleripoll <pierreripoll13@gmail.com>
2018-06-06 11:14:57 +0200

    add turn in talker.py

--- catkin_ws/src/pir/scripts/talker.py
+++ catkin_ws/src/pir/scripts/talker.py
@@ -2,19 +2,22 @@
 
 import rospy
 import math
+import numpy
 from geometry_msgs.msg import PoseStamped, Pose
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 
-DELTA_GOAL = 0.2
-SPEED_FORWARD = 0.5
-SPEED_TURN = 0.5
+DELTA_GOAL = 0.1
+DELTA_TURN = 0.02
+SPEED_FORWARD = 0.2
+SPEED_TURN = 0.8
 
 pose = Pose()
 
 def odomCallBack(msg):
 	global pose
 	pose = msg.pose.pose
+
 
 
 twistPub=rospy.Publisher("/cmd_vel_mux/input/teleop",Twist)
@@ -28,8 +31,11 @@ odomSub = rospy.Subscriber("/odom", Odometry, odomCallBack)
 def twister():
 	global pose
 	print pose
-	rospy.sleep(1)
-	forward(1)
+	while not rospy.is_shutdown():
+		rospy.sleep(0.3)
+		forward(0.3)
+		rospy.sleep(0.3)
+		turn(0.5)
 	print "TWISTER"
 
 
@@ -52,14 +58,45 @@ def forward(dist):
 		x = pose.position.x
 		y = pose.position.y
 		print "("+str(x)+","+str(y)+")"
-		if abs(xF-x)>DELTA_GOAL and abs(yF-y)>DELTA_GOAL:
+		if (abs(xF-x0)<2*DELTA_GOAL or abs(xF-x)>DELTA_GOAL) and (abs(yF-y0)<2*DELTA_GOAL or abs(yF-y)>DELTA_GOAL):
 			motion.linear.x = SPEED_FORWARD
 		else:
 			motion.linear.x = 0
 			goal = True
+			print "GOAL REACHED"
 
 		twistPub.publish(motion)
 		rospy.sleep(0.01)
+	print "END FORWARD"
+
+
+#angle en radian/PI
+def turn(angle):
+	global pose
+	print pose
+	z0 = pose.orientation.z
+	zF = pose.orientation.z +angle
+	z=pose.orientation.z
+	zcount = 0
+	goal = False
+	dz = 0
+	print "z0 ",z0," zF ", zF
+	while not goal and not rospy.is_shutdown() :
+		if numpy.sign(pose.orientation.z) == numpy.sign(z):
+			dz = abs(pose.orientation.z -z)
+		zcount += dz
+		z=pose.orientation.z
+		print "("+str(z)+")"
+		if abs(zcount-angle)>DELTA_TURN:
+			motion.angular.z = SPEED_TURN
+		else:
+			motion.angular.z = 0
+			goal = True
+			print "GOAL REACHED"
+
+		twistPub.publish(motion)
+		rospy.sleep(0.01)
+	print "END TURN"
 
 
 def forwardOld(speed,time):
